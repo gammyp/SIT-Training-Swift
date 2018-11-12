08753bb92c7b6409445247d6e5955b7836106999
EAK Apple Demo <Student26@student26s-macbook-air.local>
2018-11-12 14:15:20 +0700

    Initial Commit

---
+++ Element Quiz v.1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Element Quiz v.1.0
+//
+//  Created by Student26 on 12/11/2561 BE.
+//  Copyright © 2561 Student26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
